d791d61142e1cd70871a75d0ca66dfad1ad074bb
iKazuyoshi <kazuyoshi.inatomi@gmail.com>
2016-03-08 17:54:32 +0900

    fix some wrong scripts

--- brew.sh
+++ brew.sh
@@ -1,13 +1,25 @@
-brew update
+#!/usr/bin/env bash
+
+echo "update brew"
+# brew update
 brew upgrade
 
+echo "install ag"
 brew install ag
+echo "install autoconf"
 brew install autoconf
+echo "install automake"
 brew install automake
+echo "install docker"
 brew install docker
 brew install docker-machine
+echo "install git"
 brew install git
-brew install vim
+echo "install vim"
+brew install macvim --override-system-vim --with-cscope --with-lua
+brew linkapps macvim
+echo "install zsh"
 brew install zsh
 
 brew cleanup
+echo "done brew cleanup"
--- setup.sh
+++ setup.sh
@@ -1,44 +1,76 @@
+#!/usr/bin/env bash
+
+info() {
+  echo "[INFO] [`date "+%Y/%m/%d-%H:%M:%S"`] $1"
+}
+
+abs_dirname() {
+  local cwd="$(pwd)"
+  local path="$1"
+
+  while [ -n "$path" ]; do
+    cd "${path%/*}"
+    local name="${path##*/}"
+    path="$(readlink "$name" || true)"
+  done
+
+  pwd -P
+  cd "$cwd"
+}
+
+replace_dotfile() {
+    df=$1
+
+    info "replace dotfile: $df (`pwd`)"
+
+    [ -L "${HOME}/.${df}" ] && rm -f "${HOME}/.${df}"
+    [ -f "${HOME}/.${df}" ] && rm -f "${HOME}/.${df}"
+    ln -s `pwd`/dotfiles/${df} ~/.${df} 
+}
+
+script_dir="$(abs_dirname "$0")"
+
 # install homebrew
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew tap caskroom/cask
 brew tap Homebrew/bundle
 sh brew.sh
 
+# install Dein for Vim
+deinDir="${HOME}/.vim/dein.vim"
+mkdir -p $deinDir
+git clone https://github.com/Shougo/dein.vim.git $deinDir
+
 # set installed zsh & fish
 echo "/usr/local/bin/zsh" >> /etc/shells
 echo "/usr/local/bin/fish" >> /etc/shells
 chsh -s /usr/local/bin/zsh
 
+cd $script_dir
 # set up zplug
-curl -fLo ~/.zplug/zplug --create-dirs https://git.io/zplug
-git clone https://github.com/milkbikis/powerline-shell ~/.powerline-shell
-cd ~/.powerline-shell/;./install.sh
+#curl -fLo ~/.zplug/zplug --create-dirs https://git.io/zplug
+#git clone https://github.com/milkbikis/powerline-shell ~/.powerline-shell
+#cd ~/.powerline-shell/;./install.sh
 
+cd $script_dir
 # install nodebrew, rbenv & ruby-build, pyenv, plenv & perl-build
 curl -L git.io/nodebrew | perl - setup
-# git clone https://github.com/rbenv/rbenv.git ~/.rbenv
-# git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
-git clone https://github.com/yyuu/pyenv.git ~/.pyenv
-git clone https://github.com/tokuhirom/plenv.git ~/.plenv
-# git clone https://github.com/tokuhirom/Perl-Build.git ~/.plenv/plugins/perl-build
+# git clone https://github.com/yyuu/pyenv.git ~/.pyenv
+# git clone https://github.com/tokuhirom/plenv.git ~/.plenv
 
+cd $script_dir
 # symlinks
 if [ -f ~/.zshrc ]; then
   rm ~/.zshrc
 fi
-ln -s ~/dotfiles/.zshrc ~/.zshrc
-# ln -s ~/dotfiles/.zprofile ~/.zprofile
-# ln -s ~/dotfiles/.exports ~/.exports
-# ln -s ~/dotfiles/.aliases ~/.aliases
-# ln -s ~/dotfiles/.functions ~/.functions
-# ln -s ~/dotfiles/.gemrc ~/.gemrc
-ln -s ~/dotfiles/.vimrc ~/.vimrc
-ln -s ~/dotfiles/.gitconfig ~/.gitconfig
-# ln -s ~/dotfiles/.editorconfig ~/.editorconfig
-ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
+replace_dotfile "zshrc"
+replace_dotfile "vimrc"
+replace_dotfile "gitconfig"
+replace_dotfile "tmux.conf"
 
 # set up oh-my-zsh
-~/dotfiles/oh-my-zsh/tools/install.sh | ZSH=~/dotfiles/oh-my-zsh sh
+#~/dotfiles/oh-my-zsh/tools/install.sh | ZSH=~/dotfiles/oh-my-zsh sh
 
 # create hard-link to oh-my-zsh-powerline-theme from oh-my-zsh/themes
-ln -f ~/dotfiles/oh-my-zsh-powerline-theme/powerline.zsh-theme ~/dotfiles/oh-my-zsh/themes/powerline.zsh-theme
+#ln -f ~/dotfiles/oh-my-zsh-powerline-theme/powerline.zsh-theme ~/dotfiles/oh-my-zsh/themes/powerline.zsh-theme
+
